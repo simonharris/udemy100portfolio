8fb4b73f78881d94adc3f7ca9305055bda8097e1
Simon H <github@pointbeing.net>
2025-04-28 02:08:10 +0100

    Complete the task; lint to 10.0

--- 080-MorseEncoder/morse.py
+++ 080-MorseEncoder/morse.py
@@ -1,16 +1,25 @@
 """
 Requirements:
-You will use what you've learnt to create a text-based (command line) program 
+You will use what you've learnt to create a text-based (command line) program
 that takes any String input and converts it into Morse Code.
 """
 from data import MORSE_CODE
 
-def encode(input: str) -> str:
-    
 
-    output = [MORSE_CODE[char.upper()] for char in input]
+def encode(plaintext: str) -> str:
+    """Encode plain text to Morse code"""
 
-    return ''.join(output)
-
+    return ''.join([MORSE_CODE[char.upper()] for char in plaintext])
 
 
+# Main ------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+
+    plain = input('Please enter a string to encode: ')
+    try:
+        encoded = encode(plain)
+        print(f"In Morse, that is: {encoded}")
+    except KeyError as ex:
+        print(f"Sadly, {ex} cannot currently be encoded")
--- 080-MorseEncoder/tests/test_morse.py
+++ 080-MorseEncoder/tests/test_morse.py
@@ -7,5 +7,13 @@ class MorseTest(unittest.TestCase):
     def test_phases(self):
         self.assertEqual(encode('s'), '...')
         self.assertEqual(encode('sos'), '...---...')
-        self.assertEqual(encode('2025/04/28'), 
+        self.assertEqual(encode('2025/04/28'),
                          '..--------..---.....-..-.-----....--..-...------..')
+
+    def test_case_insensitive(self):
+        self.assertEqual(encode('morsecode'), '-----.-.....-.-.----...')
+        self.assertEqual(encode('mOrSeCoDe'), '-----.-.....-.-.----...')
+
+    def test_missing_characters(self):
+        with self.assertRaises(Exception) as _:
+            encode('Rod, Jane & Freddy')
