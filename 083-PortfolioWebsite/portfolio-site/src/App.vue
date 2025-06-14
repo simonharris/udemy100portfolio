836412f51b04bb0c80af94225dcafd94073742d6
Simon H <github@pointbeing.net>
2025-06-14 12:45:29 +0100

    A few cosmetic tweaks. Still looks awful, obviously

--- 083-PortfolioWebsite/portfolio-site/src/App.vue
+++ 083-PortfolioWebsite/portfolio-site/src/App.vue
@@ -3,26 +3,35 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="row">
-    <h1><router-link to="/">Simon's Page</router-link></h1>
-  </div>
 
+<div class="container-fluid item-block-in">
+  <div class="container-lg">
+        <div class="row pt-4">
+          <h1 class="display-2"><router-link to="/">Simon's Page</router-link></h1>
+          <p class="lead">Some placeholder text and maybe a link or two</p>
+        </div>
+      </div>
+</div>
 
+<div class="container-lg">
 <div class="row">
 <div v-for="project in projects" class="p-1 col-sm-6 col-md-4 col-lg-3">
 
-    <div class="item-block-in">
+    <div class="position-relative">
       <router-link :to="'/' + project.slug">
       <img :src="'http://127.0.0.1:5000/static/thumbs/' + project.thumbnail" class="img-fluid img-thumbnail mb-2">
       </router-link>
 
-      <div class="p-2">
-        <h4>
-            <router-link :to="'/' + project.slug">{{ project.name }}</router-link>
-        </h4>
-        <p>{{  project.headline }}</p>
+      <div class="position-absolute bottom-0 start-0 bg-secondary w-100 p-2" style="--bs-bg-opacity: .8">
+
+      <h4 class="fs-6">
+            <router-link style="color: white; text-decoration: none;" :to="'/' + project.slug">{{ project.name }}</router-link>
+      </h4>
+      <p class="fs-6">{{  project.headline }}</p>
+
       </div>
     </div>
+</div>
 </div>
 </div>
 
