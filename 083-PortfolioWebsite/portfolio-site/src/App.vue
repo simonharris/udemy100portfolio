ea7ef063476d745ef689cd24a936431237576280
Simon H <github@pointbeing.net>
2025-06-15 08:35:04 +0100

    Add basic transition

--- 083-PortfolioWebsite/portfolio-site/src/App.vue
+++ 083-PortfolioWebsite/portfolio-site/src/App.vue
@@ -7,17 +7,32 @@ import CommonFooter from './components/CommonFooter.vue'
 
 <div class="container-fluid item-block-in">
   <div class="container-lg">
-        <div class="row pt-4">
-          <h1 class="display-2"><router-link to="/">Simon's Page</router-link></h1>
-          <p class="lead">Some placeholder text and maybe a link or two</p>
-        </div>
-      </div>
+    <div class="row pt-4">
+      <h1 class="display-2"><router-link to="/">Simon's Page</router-link></h1>
+      <p class="lead">Some placeholder text and maybe a link or two</p>
+    </div>
+  </div>
 </div>
 
 <div class="container-lg">
-  <RouterView />
+  <transition name="fade" mode="out-in">
+    <RouterView />
+  </transition>
 </div>
 
 <CommonFooter />
 
 </template>
+
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
