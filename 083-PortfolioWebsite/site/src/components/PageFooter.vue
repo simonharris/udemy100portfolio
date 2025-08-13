1c0b0942fa6a1d7e911db165340e27c3b9a7fb2f
Simon H <github@pointbeing.net>
2025-08-13 19:25:48 +0100

    Various front end refinements

--- 083-PortfolioWebsite/site/src/components/PageFooter.vue
+++ 083-PortfolioWebsite/site/src/components/PageFooter.vue
@@ -1,15 +1,16 @@
 <template>
 
-<div class="container-fluid ">
+<div class="container-fluid p-0">
 <div class="container-lg">
-<hr>
+<hr class="mx-0">
 <footer class="row">
-   <div class="col-md-6 d-flex align-items-center">
+   <div class="col-sm-6 d-flex align-items-center">
         <span>&copy; 2025 <a href="https://simon.colchester.social/">Simon Harding</a>.</span>
     </div>
-    <div class="col-md-6 text-end fs-4">
-        <a href="https://simon.colchester.social"><i class="me-md-2 bi bi-bluesky"></i></a>
-        <a href="https://github.com/simonharris"><i class="bi bi-github"></i></a>
+    <div class="col-sm-6 text-sm-end fs-4">
+        <a href="https://simon.colchester.social"><i class="me-2 bi bi-bluesky"></i></a>
+        <a href="https://github.com/simonharris"><i class="me-2 bi bi-github"></i></a>
+        <a href="mailto:projects@pointbeing.net "><i class="bi bi-envelope-fill"></i></a>
     </div>
 </footer>
 <hr>
--- 083-PortfolioWebsite/site/src/components/PageHeader.vue
+++ 083-PortfolioWebsite/site/src/components/PageHeader.vue
@@ -1,8 +1,11 @@
 
 <template>
-<div class="container-fluid">
+<div class="container-fluid p-0">
   <div class="container-lg">
-        <hr>
+     <div class="col fs-1 pt-2">
+        <a href="/" title="Projects Home"><i class="bi bi-house-fill"></i></a>
+     </div>
+        <hr class="mt-0">
     <div class="row pt-4">
       <h1 class="display-2"><router-link to="/">{{ config.title }}</router-link></h1>
 
